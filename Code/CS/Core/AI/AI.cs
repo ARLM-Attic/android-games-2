0213ae6925f3794fd607bd1f619a1b232f2401ac
SND\ly_jaeho_cp <SND\ly_jaeho_cp@0ce29038-4a70-45ae-8a1e-7d2f2ce429ee>
2013-05-13 07:10:13 +0000

    git-svn-id: https://ag.svn.codeplex.com/svn@26800 0ce29038-4a70-45ae-8a1e-7d2f2ce429ee

---
+++ Code/CS/Core/AI/AI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AI
+{
+    public static void Run(Map2D map, Camp camp)
+    {
+        for (int iUnit = 0; iUnit < camp.AvailableUnitList.Count; iUnit++)
+        {
+            if (camp.Income >= camp.AvailableUnitList[iUnit].CostM && camp.Population < camp.PopulationLimit)
+            {
+                //if (camp.Income >= camp.AvailableUnitList[iUnit].CostM && camp.Population <2)
+                //{
+                Object2D obj = AGSUtility.CreateObject(map, camp, DATUtility.GetUnit(camp.AvailableUnitList[iUnit].Id), "unknown", camp.StartPos, Direction2DDef.South.Id);
+                AGSUtility.MoveTo(obj, map.Camps[0].StartPos);
+            }
+        }
+    }
+}
