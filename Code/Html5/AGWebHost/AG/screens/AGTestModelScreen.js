d609c2fe0ffb9d49565b31de12bcc01516d6c383
SND\string_bao_cp <SND\string_bao_cp@0ce29038-4a70-45ae-8a1e-7d2f2ce429ee>
2015-03-13 05:47:21 +0000

    git-svn-id: https://ag.svn.codeplex.com/svn@40215 0ce29038-4a70-45ae-8a1e-7d2f2ce429ee

--- Code/Html5/AGWebHost/AG/screens/AGTestModelScreen.js
+++ Code/Html5/AGWebHost/AG/screens/AGTestModelScreen.js
@@ -3,25 +3,23 @@
     this.init = function (engine) {
         this._model = engine._resLoader.loadModel(1);
     }
+        
+
+    this.x = 10;
+    this.y = 10;
+    this.t = new Date().getSeconds();
 
     this.render = function (engine) {
-        for (var i = 0; i < 900; i++) {
-            engine._gdi.draw(engine._images[0], i * 10, i * 10);
-            engine._gdi.draw(engine._images[1], i * 20, i * 20);
-        }
-        //        var halfW = 40;
-        //        var halfH = 20;
-        //        for (var r = 0; r < 2; r++) {
-        //            for (var c = 0; c < 1; c++) {
+        var _t = new Date().getSeconds();
 
-        //                var x = (r + c) * 40;
-        //                var y = (r - c) * 20;
-        //                engine._gdi.drawDiamond(x, y, 80, 40);
-        //                engine._gdi.drawString("(" + r + "," + c + ")", x + 10, y + 10);
-        //            }
-        //        }
-        // int col = (int)(ptInMap.X / MapCell.Width - ptInMap.Y / MapCell.Height);
-        // int row = (int)(ptInMap.X / MapCell.Width + ptInMap.Y / MapCell.Height);
+        var deltaT = _t - this.t;
+        if (deltaT > 0) {
+            this.t = _t;
+
+            this.x = this.x + 30 * deltaT;
+        }
+
+            engine._gdi.drawDiamond(this.x, this.y, 50, 50);
     }
 
     this.loop = function (engine) {
